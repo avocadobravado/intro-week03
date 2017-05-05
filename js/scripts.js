c2675accf043bcc0919457a062c6dccbe0c093f6
Rose Sponseller <rosesponseller@gmail.com>
2017-05-05 10:48:54 -0700

    User input will display individual numbers in li when submitted

--- js/scripts.js
+++ js/scripts.js
@@ -15,24 +15,17 @@ var pingPong = function(count) {
   return result;
 };
 
-
-
 // Front end logic
 $(function() {
   $("#pingPongForm").submit(function(event) {
-    // Will allow results to display on page
     event.preventDefault();
-    // Gather user input and convert to number
     var userInput = parseInt($("input#userInput").val());
-    // Call pingPong function and pass in userInput as argument
     var input = pingPong(userInput);
-    console.log(input);
-
-  //   $("#userInput").text(input);
-   //
-   //
-
-   //
-  //   $("#result").append("<li>" + input + "</li>");
+    // Empty method removes all existing children in the DOM, allowing user to enter a new number and see new results multiple times
+    $("#result").empty();
+    input.forEach(function(num) {
+      $("#result").append("<li>" + num + "</li>");
+    });
+    $("#result").fadeIn("slow");
   });
 });
